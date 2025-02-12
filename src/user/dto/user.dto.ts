6922da5f2049914e28ec6deb570163bb9f2bc5bc
miz.mary <simk.otar@gmail.com>
2025-02-12 20:38:27 +0300

    registration, authorisation; user entity

---
+++ src/user/dto/user.dto.ts
@@ -0,0 +1,40 @@
+import { IsEmail, IsOptional, IsString, MinLength } from "class-validator"
+
+export class AboutUser {
+  @IsOptional()
+  bio?: string
+
+  @IsOptional()
+  tags?: string[]
+
+  @IsOptional()
+  shirtSizes?: string[]
+
+  @IsOptional()
+  shoeSizes?: string[]
+
+  @IsOptional()
+  ringSizes?: string[]
+
+  @IsOptional()
+  braceletSizes?: string[]
+
+  @IsOptional()
+  necklaceSizes?: string[]
+}
+
+export class UserDto extends AboutUser {
+  @IsOptional()
+  @IsEmail()
+  email?: string
+
+  @IsOptional()
+  @MinLength(8, {
+    message: "Password must be at least 6 characters long"
+  })
+  @IsString()
+  password?: string
+
+  @IsOptional()
+  name?: string
+}
